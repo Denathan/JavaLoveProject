e70cb082cca8f239c723db8efb07a4d09e9b1884
Daniel <danielrodak27@gmail.com>
2018-04-16 00:01:12 +0200

    Added many operations and history.

--- src/pl/drodak/Main.java
+++ src/pl/drodak/Main.java
@@ -1,16 +1,26 @@
 package pl.drodak;
 
 import java.util.Scanner;
+import java.util.*;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+//import  java.io.*;
+//import  org.apache.poi.hssf.usermodel.HSSFSheet;
+//import  org.apache.poi.hssf.usermodel.HSSFWorkbook;
+//import  org.apache.poi.hssf.usermodel.HSSFRow;
 
 public class Main {
     static String pierwszyWybor, kalkulatorWybor;
     static Scanner reader = new Scanner(System.in);
+    static private List<String> operacje = new ArrayList<>();
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ScriptException {
         menu();
     }
 
-    private static void menu() {
+    private static void menu() throws ScriptException {
         System.out.println("****************************************");
         System.out.println("*             I LOVE JAVA              *");
         System.out.println("****************************************");
@@ -25,7 +35,7 @@ public class Main {
         pierwszyWybor = reader.nextLine();
     }
 
-    private static void checkOption() {
+    private static void checkOption() throws ScriptException {
         String n;
         switch (pierwszyWybor) {
             case ("1"):
@@ -56,7 +66,7 @@ public class Main {
         kalkulatorWybor = reader.nextLine();
     }
 
-    private static void calculatorMenu() {
+    private static void calculatorMenu() throws ScriptException {
         Scanner input = new Scanner(System.in);
         System.out.println("****************************************");
         System.out.println("*                 MENU                 *");
@@ -67,9 +77,11 @@ public class Main {
         System.out.println("4. Iloraz");
         System.out.println("5. Pierwiastek n-tego stopnia");
         System.out.println("6. Potęga");
+        System.out.println("7. Proste działanie wpisane przez Ciebie");
 
         checkCalculatorOption();
         double a, b, wynik;
+        String aSt, bSt, wynikSt;
 
         switch (kalkulatorWybor) {
             case ("1"):
@@ -83,6 +95,10 @@ public class Main {
                 wynik = Math.round(wynik);
                 wynik /= 100;
                 System.out.println("Suma liczb wynosi " + wynik);
+                aSt = String.valueOf(a);
+                bSt = String.valueOf(b);
+                wynikSt = String.valueOf(wynik);
+                operacje.add(aSt + " + " + bSt + " = " + wynikSt);
                 endOrNot();
                 break;
             case ("2"):
@@ -96,6 +112,10 @@ public class Main {
                 wynik = Math.round(wynik);
                 wynik /= 100;
                 System.out.println("Różnica liczb wynosi " + wynik);
+                aSt = String.valueOf(a);
+                bSt = String.valueOf(b);
+                wynikSt = String.valueOf(wynik);
+                operacje.add(aSt + " - " + bSt + " = " + wynikSt);
                 endOrNot();
                 break;
             case ("3"):
@@ -109,6 +129,10 @@ public class Main {
                 wynik = Math.round(wynik);
                 wynik /= 100;
                 System.out.println("Iloczyn liczb wynosi " + wynik);
+                aSt = String.valueOf(a);
+                bSt = String.valueOf(b);
+                wynikSt = String.valueOf(wynik);
+                operacje.add(aSt + " * " + bSt + " = " + wynikSt);
                 endOrNot();
                 break;
             case ("4"):
@@ -126,6 +150,10 @@ public class Main {
                     wynik = Math.round(wynik);
                     wynik /= 100;
                     System.out.println("Iloraz liczb wynosi " + wynik);
+                    aSt = String.valueOf(a);
+                    bSt = String.valueOf(b);
+                    wynikSt = String.valueOf(wynik);
+                    operacje.add(aSt + " / " + bSt + " = " + wynikSt);
                     endOrNot();
                 }
                 break;
@@ -138,6 +166,10 @@ public class Main {
                 double stopien = c / b;
                 wynik = Math.pow(a, stopien);
                 System.out.println("Pierwiatek "+ b +"-ego stopnia z liczby "+ a +" to "+ wynik);
+                aSt = String.valueOf(a);
+                bSt = String.valueOf(b);
+                wynikSt = String.valueOf(wynik);
+                operacje.add("sqrt[" + bSt + "](" + aSt + ") = " + wynikSt);
                 endOrNot();
                 break;
             case ("6"):
@@ -147,17 +179,35 @@ public class Main {
                 b = input.nextDouble();
                 wynik = Math.pow(a, b);
                 System.out.println("Liczba "+ a +" podniesiona do potegi "+ b +" to "+ wynik);
+                aSt = String.valueOf(a);
+                bSt = String.valueOf(b);
+                wynikSt = String.valueOf(wynik);
+                operacje.add(aSt + "^" + bSt + " = " + wynikSt);
+                endOrNot();
+                break;
+            case ("7"):
+                System.out.println("Wpisz działanie.");
+                ScriptEngineManager mgr = new ScriptEngineManager();
+                ScriptEngine engine = mgr.getEngineByName("JavaScript");
+                String foo;
+                foo = reader.nextLine();
+                System.out.println("Wynik działania to: " + engine.eval(foo));
+                aSt = String.valueOf(foo);
+                wynikSt = String.valueOf(engine.eval(foo));
+                operacje.add(aSt + " = " + wynikSt);
                 endOrNot();
                 break;
             default:
-                System.out.println("Nieprawidłowy wybór. Wybierz 1, 2, 3, 4, 5 lub 6.");
+                System.out.println("Nieprawidłowy wybór. Wybierz 1, 2, 3, 4, 5, 6 lub 7.");
                 calculatorMenu();
+
+
         }
     }
 
-    private static void endOrNot() {
+    private static void endOrNot() throws ScriptException {
         String n;
-        System.out.println("Wpisz start, aby powrócić do początku lub end, aby zakończyć działanie programu.");
+        System.out.println("Wpisz start, aby powrócić do początku, historia aby uzyskać historię wykonanych operacji lub end, aby zakończyć działanie programu.");
         n = reader.nextLine();
         switch (n) {
             case ("start"):
@@ -165,6 +215,10 @@ public class Main {
                 break;
             case ("end"):
                 System.exit(0);
+                break;
+            case ("historia"):
+                System.out.println(operacje );
+                endOrNot();
                 break;
             default:
                 System.out.println("Nieprawidłowy wybór. Wpisz start lub end.");
